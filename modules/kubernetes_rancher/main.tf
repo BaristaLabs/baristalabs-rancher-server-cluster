91b46d3e564d7f8e4b57f17564b28dd30c7e370c
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-06-03 00:17:46 -0400

    Upgrade Rancher to 2.6.5

--- modules/kubernetes_rancher/main.tf
+++ modules/kubernetes_rancher/main.tf
@@ -2,9 +2,11 @@ resource "helm_release" "rancher" {
   name       = "rancher"
   repository = "https://releases.rancher.com/server-charts/stable"
   chart      = "rancher"
-  version    = "2.6.4"
+  version    = "2.6.5"
 
   namespace = var.rancher_namespace
+
+  reuse_values = true
 
   values = [
     file("${path.module}/values/rancher_values.yaml")
